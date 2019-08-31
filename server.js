47558b2b3a0f97efaaae4961e0c8b1b305f1de47
thegreataxios <sawyer.cutler@gmail.com>
2019-08-30 20:53:57 -0500

    License Update to MIT

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 'use strict';
 
-const app = require('./app');
+const app = require('./app'); // Require the app.js file in Root
 
 // Config
-const config = require('./app/config/config');
+const config = require('./app/config/config'); // Require Basic Config for PORT
 
-let port = config.app.port;
-app.listen(port, (err) => {
+let port = config.app.port; // Init Port
+app.listen(port, (err) => { // Express Version of Server Create
 	if (err) {
-		console.log(err);
+		console.log(err); // Can remove for production
 		return server.status(0);
 	} else {
-		console.log('Server Running on Port ' + port);
+		console.log('Server Running on Port ' + port); // Can remove for production
 	}
 });
